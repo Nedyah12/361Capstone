53bd61eb3cf32e6abac340a11ce7eab84aa61564
Isa <iadrovergallego2@huskers.unl.edu>
2021-12-05 19:38:50 -0800

    Added Controllers

---
+++ 361Capstone/361Capstone/Controllers/CartController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using _361Capstone.Models;
+using _Capstone.Manager;
+using _Capstone.Engine;
+using OnlineStore.Accessors;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _361Capstone.Controllers
+{
+    public class CartController : Controller
+    {
+        private LoginManager LoginManager { get; set; } = new LoginManager();
+        private ListManager ListManager { get; set; } = new ListManager();
+
+        public ActionResult Index(int CartId)
+        {
+            Cart cart = ListManager.GetCart(CartId);
+            int UserId = cart.getUserId();
+
+            //Should we authenticate the login?
+
+            ViewData["UserId"] = cart.getUserId();
+            //GetProducts in cart model? 
+            ViewData["Products"] = 
+            ViewData["CartId"] = CartId;
+
+            return View();
+        }
+        
+        public ActionResult Add(int ProductId, int CartId)
+        {
+            Cart cart = ListManager.GetCart(CartId);
+            int userId = cart.getUserId();
+            //Authenticate Login
+
+            //Product product = new Product();??
+            ListManager.AddProducts(product);
+
+            return RedirectToAction("Index", new { CartId });
+        }
+
+        public ActionResult Edit(int CartId, int ProductId, string NewProduct)
+        {
+
+        }
+
+        public ActionResult Delete(int ProductId, int CartId)
+        {
+            Cart cart = ListManager.GetCart(CartId);
+            int userId = cart.getUserId();
+            //Authenticate Login
+
+            //Product poduct = new Product();???
+            ListManager.RemoveProduct(product);
+
+        }
+        
+
+        
+       
+    }
+}
---
+++ 361Capstone/361Capstone/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using _361Capstone.Models;
+using _Capstone.Manager;
+using _Capstone.Engine;
+using OnlineStore.Accessors;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _361Capstone.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index(bool incorrectPassword = false, bool incorrectUsername = false)
+        {
+            ViewData["IncorrectPassword"] = incorrectPassword;
+            ViewData["IncorrectUsername"] = incorrectUsername;
+            return View();
+        }
+
+        public ActionResult SuccessfulLogin(int userId)
+        {
+
+        }
+
+        public ActionResult AuthenticateUser(string username, string password)
+        {
+            bool incorrectPassword = false;
+            bool incorrectUsername = false;
+
+            if (password == null)
+            {
+                incorrectPassword = true;
+                return RedirectToAction("Index", new { incorrectPassword, incorrectUsername });
+            }
+
+            else if (username == null)
+            {
+                incorrectUsername = true;
+                return RedirectToAction("Index", new { incorrectPassword, incorrectUsername });
+            }
+
+            //need the security manager to handle passwords
+            return RedirectToAction("Index", new { incorrectPassword, incorrectUsername });
+        }
+
+        public ActionResult Logout()
+        {
+            //Should have Login manager ready to implement this
+            
+            
+        }
+    }
+}
