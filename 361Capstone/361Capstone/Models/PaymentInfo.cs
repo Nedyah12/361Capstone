ea0520e62803ad1e7987248bc9b5ce6227c85a8a
Jett <jettdurante54@icloud.com>
2021-12-07 16:36:46 -0600

    payment info model

---
+++ 361Capstone/361Capstone/Models/PaymentInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace _361Capstone.Models
+{
+    public class PaymentInfo : IPaymentInfo
+    {
+        private int PaymentInfoId { get; set; }
+        private int UserId { get; set; }
+        private string CardNumber { get; set; }
+        private int ExpirationMonth { get; set; }
+        private int ExpirationYear { get; set; }
+        private int Cvv { get; set; }
+
+        public PaymentInfo(int payInfoId, int userId, string cardNumber, int month, int year, int cvv)
+        {
+            PaymentInfoId = payInfoId;
+            UserId = userId;
+            CardNumber = cardNumber;
+            ExpirationMonth = month;
+            ExpirationYear = year;
+            Cvv = cvv;
+        }
+
+        public int GetPaymentInfoId()
+        {
+            return PaymentInfoId;
+        }
+        public int GetUserId()
+        {
+            return UserId;
+        }
+        public string GetCardNumber()
+        {
+            return CardNumber;
+        }
+        public int GetExpirationMonth()
+        {
+            return ExpirationMonth;
+        }
+        public int GetCvv()
+        {
+            return Cvv;
+        }
+    }
+}
