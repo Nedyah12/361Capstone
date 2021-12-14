9e77a537bef8143b1aa945fb4c1d47345eb7c6ae
Isa <iadrovergallego2@huskers.unl.edu>
2021-12-13 21:07:42 -0800

    Added Unit Tests

---
+++ 361Capstone/Tests/UnitTests/CheckoutEngineTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Tests.UnitTests
+{
+    [TestClass]
+    public class CheckoutEngineTest
+    {
+        //arrange
+        _361Capstone.Engine.CheckoutEngine CheckoutEngine = new _361Capstone.Engine.CheckoutEngine();
+
+        [TestMethod]
+        public void GetPaymentInfo3_Pass()
+        {
+            //act
+            List<String> PaymentInfo = CheckoutEngine.GetPaymentInfo(3);
+
+            //assert
+            Assert.AreNotEqual(-1, PaymentInfo);
+
+        }
+
+        [TestMethod]
+        public void GetPaymentInfo12_Pass()
+        {
+            //act
+            List<String> PaymentInfo = CheckoutEngine.GetPaymentInfo(12);
+
+            //assert
+            Assert.AreNotEqual(-1, PaymentInfo);
+
+        }
+    }
+
+}
---
+++ 361Capstone/Tests/UnitTests/LoginEngineTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.UnitTests
+{
+    [TestClass]
+    public class LoginEngineTests
+    {
+        //arrange
+        _361Capstone.Engine.LoginEngine engine = new _361Capstone.Engine.LoginEngine();
+
+
+        [TestMethod]
+        public void CheckUsernameJW_Pass()
+        {
+            //act
+            int result = engine.CheckUsername("Jade Walker");
+
+            //assert
+            Assert.AreNotEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void CheckUsernameP_Pass()
+        {
+            //act
+            int result = engine.CheckUsername("P");
+
+            //assert
+            Assert.AreNotEqual(-1, result);
+        }
+    }
+}
