3cb76ee4968439836d408f18cf4bf2059b039610
Jett <jettdurante54@icloud.com>
2021-12-13 21:29:27 -0600

    large unit testing refactoring, new folders and everything

---
+++ 361Capstone/Tests/UnitTests/ProductManagerTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class ProductManagerTest
+    {
+        //arrange
+        _361Capstone.Managers.ProductsManager productsManager = new _361Capstone.Managers.ProductsManager();
+
+        [TestMethod]
+        public void GetProduct10_Pass()
+        {
+            
+            //act
+            _361Capstone.Models.Product product = productsManager.GetProduct(10);
+            //assert
+            Assert.AreEqual(product.GetProductId(), 10);
+            
+        }
+
+        [TestMethod]
+        public void GetProduct3_Pass()
+        {
+           
+            //act
+            _361Capstone.Models.Product product = productsManager.GetProduct(3);
+            //assert
+            Assert.AreEqual(product.GetProductId(), 3);
+
+        }
+
+        [TestMethod]
+        public void GetProduct16_Pass()
+        {
+           
+            //act
+            _361Capstone.Models.Product product = productsManager.GetProduct(16);
+            //assert
+            Assert.AreEqual(product.GetProductId(), 16);
+
+        }
+    }
+}
